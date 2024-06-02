e148c97eb1782e2b6ac27736d61a6db2745f6703
Gerard <gersapinads@gmail.com>
2024-06-02 17:45:11 +0800

    Todo: user login verification

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -10,6 +10,7 @@ module.exports.registerUser = async (req, res) => {
         const isTaken = await User.findOne({username:username})
         if (isTaken) {
             res.status(201).json({message: "Username Already taken"})
+            return
         }
 
         const user = {
@@ -20,6 +21,21 @@ module.exports.registerUser = async (req, res) => {
         await User.create(user)
         res.status(201).json(`User Successfully Registered`)
 
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+}
+
+module.exports.verifyLogin = async (req,res) => {
+    try {
+        const {username, password} = req.body
+
+        // 1. find matching username 
+        // 2. get username's object
+        // 3. decrypt obj's password
+        // 4. evaluate req.body's password to decrypted password
+        // 5. isSuccess ? createToken : verification failed
+        
     } catch (error) {
         res.status(500).json({message:error.message})
     }
