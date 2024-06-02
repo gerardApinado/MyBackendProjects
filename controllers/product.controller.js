53fb2965bd47f55a5228f2575f54b9945a38a743
Gerard <gersapinads@gmail.com>
2024-06-02 16:02:08 +0800

    add product controller

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,2 +1,53 @@
 const Product = require('../models/product.model')
 
+
+module.exports.createProduct = async (reqBody) => {
+    // .create() to create data from body
+    const product = await Product.create(reqBody).then((result, err) => result ? result : err)
+
+    return product
+}
+
+module.exports.getAllProducts = async () => {
+    // .find to find all data
+    const allProducts = await Product.find({}).then((result) => result)
+
+    return allProducts
+}
+
+module.exports.getProductByID = async (id) => {
+    // .find to find all data
+    const productById = await Product.findById(id).then((result) => result )
+
+    return productById
+}
+
+// code camp verison controller
+module.exports.updateProductByID = async (req,res) => {
+    try {
+        const productUpdateById = await Product.findByIdAndUpdate(req.params.id, req.body)
+
+        if (!productUpdateById) {
+            res.status(404).json({message: "Product not found."})
+        }
+
+        const updatedProduct = await Product.findById(req.params.id)
+        res.status(200).json(updatedProduct)
+
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
+
+module.exports.deleteProductByID = async (req, res) => {
+    try {
+        const deleteByID = await Product.findByIdAndDelete(req.params.id)
+        if (!deleteByID){
+            res.status(404).json({message: "Product not found."})
+        }
+
+        res.status(200).json({message: `Product with ID:${req.params.id} deleted`})
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
--- routes/product.route.js
+++ routes/product.route.js
@@ -2,14 +2,14 @@ const express = require('express')
 
 const Product = require('../models/product.model')
 
+const ProductController = require('../controllers/product.controller')
+
 const router = express.Router()
 
 // API HTTP Requests
 router.post('/createProduct', async (req,res) => {
     try {
-        // .create() to create data from body
-        const product = await Product.create(req.body)
-        res.status(200).json(product)
+        ProductController.createProduct(req.body).then(result => res.status(201).send(result))
     } catch (error) {
         res.status(500).json({message: error.message})
     }
@@ -18,9 +18,7 @@ router.post('/createProduct', async (req,res) => {
 // find all
 router.get('/getProducts', async (req,res) => {
     try {
-        // .find to find all data
-        const allProducts = await Product.find({})
-        res.status(200).json(allProducts)
+        ProductController.getAllProducts().then(result => res.status(200).send(result))
     } catch (error) {
         res.status(500).json({message: error.message})
     }
@@ -29,42 +27,16 @@ router.get('/getProducts', async (req,res) => {
 // find by ID
 router.get('/getProduct/:id', async (req,res) => {
     try {
-        // .find to find all data
-        const productById = await Product.findById(req.params.id)
-        res.status(200).json(productById)
+        ProductController.getProductByID(req.params.id).then(result => res.status(200).send(result))
     } catch (error) {
         res.status(500).json({message: error.message})
     }
 })
 
-// update
-router.put('/updateProduct/:id', async (req, res) => {
-    try {
-        const productUpdateById = await Product.findByIdAndUpdate(req.params.id, req.body)
-
-        if (!productUpdateById) {
-            res.status(404).json({message: "Product not found."})
-        }
-
-        const updatedProduct = await Product.findById(req.params.id)
-        res.status(200).json(updatedProduct)
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
+// update - code camp version
+router.put('/updateProduct/:id', ProductController.updateProductByID)
 
 // delete
-router.delete('/deleteProduct/:id', async (req, res) => {
-    try {
-        const deleteByID = await Product.findByIdAndDelete(req.params.id)
-        if (!deleteByID){
-            res.status(404).json({message: "Product not found."})
-        }
-
-        res.status(200).json({message: `Product with ID:${req.params.id} deleted`})
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
+router.delete('/deleteProduct/:id', ProductController.deleteProductByID)
 
 module.exports = router
